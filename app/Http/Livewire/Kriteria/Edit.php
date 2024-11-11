0f707894643002b041f0b125d53d9045c9a2dc40
adamjatim <adamrahmatilahi@gmail.com>
2024-11-11 22:17:04 +0700

    update Edit.php

--- app/Http/Livewire/Kriteria/Edit.php
+++ app/Http/Livewire/Kriteria/Edit.php
@@ -3,62 +3,54 @@
 namespace App\Http\Livewire\Kriteria;
 
 use App\Models\Kriteria;
-use Illuminate\Support\Facades\Validator;
 use Livewire\Component;
 
 class Edit extends Component
 {
-	public $kriteria, $kode, $nama, $bobot, $persen, $keterangan;
+  public $kriteria;
+  public $kode_kriteria, $nama_kriteria, $nilai_bobot, $persentase, $keterangan;
 
-	public function mount($id)
-	{
-		$this->kriteria = Kriteria::find($id);
-
-    $this->kode = $this->kriteria->kode_kriteria;
-    $this->nama = $this->kriteria->nama_kriteria;
-    $this->bobot = $this->kriteria->nilai_bobot;
-    $this->persen = $this->kriteria->persentase;
+  public function mount($id)
+  {
+    $this->kriteria = Kriteria::find($id);
+    $this->kode_kriteria = $this->kriteria->kode_kriteria;
+    $this->nama_kriteria = $this->kriteria->nama_kriteria;
+    $this->nilai_bobot = $this->kriteria->nilai_bobot;
+    $this->persentase = $this->kriteria->persentase;
     $this->keterangan = $this->kriteria->keterangan;
-	}
+  }
 
-  protected $rules = [
-    'kode'        => 'required|string',
-    'nama'        => 'required',
-    'bobot'       => 'required',
-    'persen'      => 'required|integer',
-    'keterangan'  => 'required',
-  ];
+  public function update()
+  {
+    $this->validate([
+      'kode_kriteria' => 'required|string',
+      'nama_kriteria' => 'required',
+      'nilai_bobot' => 'required',
+      'persentase' => 'required|numeric',
+      'keterangan' => 'required',
+    ], [
+      'kode.required'           => 'Kode kriteria wajib diisi.',
+      'kode.string'             => 'Kode kriteria harus berupa string.',
+      'nama.required'           => 'Nama kriteria wajib diisi.',
+      'bobot.required'          => 'Bobot kriteria wajib diisi. (format: 0.<angka>)',
+      'persen.required'         => 'Persentase wajib diisi.',
+      'persen.integer'          => 'Persentase harus berupa angka.',
+      'keterangan.required'     => 'Keterangan wajib dipilih.',
+    ]);
 
-  protected $messages = [
-    'kode.required'       => 'Kode kriteria wajib diisi.',
-    'kode.string'         => 'Kode kriteria harus berupa string.',
-    'nama.required'       => 'Nama kriteria wajib diisi.',
-    'bobot.required'      => 'Bobot kriteria wajib diisi. (format: 0.<angka>)',
-    'persen.required'     => 'Persentase wajib diisi.',
-    'persen.integer'      => 'Persentase harus berupa angka.',
-    'keterangan.required' => 'Keterangan wajib dipilih.',
-  ];
-
-
-
-	public function render()
-	{
-		return view('livewire.kriteria.edit');
-	}
-
-	public function update()
-	{
-    // Validasi input
-    $this->validate();
-
-		$this->kriteria->update([
-      'kode_kriteria' => $this->kode,
-      'nama_kriteria' => $this->nama,
-      'nilai_bobot' => $this->bobot,
-      'persentase' => $this->persen,
+    $this->kriteria->update([
+      'kode_kriteria' => $this->kode_kriteria,
+      'nama_kriteria' => $this->nama_kriteria,
+      'nilai_bobot' => $this->nilai_bobot,
+      'persentase' => $this->persentase,
       'keterangan' => $this->keterangan,
     ]);
 
-		return redirect()->route('kriteria.index');
-	}
+    return redirect()->route('kriteria.index')->with('message', 'Kriteria berhasil diubah.');
+  }
+
+  public function render()
+  {
+    return view('livewire.kriteria.edit');
+  }
 }
