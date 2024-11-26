6b13b9f5296c2a8c1b70173e68271b7b366c7a61
adamjatim <adamrahmatilahi@gmail.com>
2024-11-27 00:51:02 +0700

    update 2024_10_24_131850_create_sub_kriterias_table.php

--- database/migrations/2024_10_24_131850_create_sub_kriterias_table.php
+++ database/migrations/2024_10_24_131850_create_sub_kriterias_table.php
@@ -1,27 +1,31 @@
 <?php
 
-// database/migrations/xxxx_xx_xx_create_sub_bkriterias_table.php
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class CreateSubKriteriasTable extends Migration
 {
-    public function up()
-    {
-        Schema::create('sub_kriterias', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('kode_kriteria'); // ID kriteria utama
-            $table->string('parameter');        // Nama sub kriteria
-            $table->integer('nilai');                  // Nilai sub kriteria
-            $table->timestamps();
+  public function up()
+  {
+    Schema::create('sub_kriterias', function (Blueprint $table) {
+      $table->id(); // Primary key
+      $table->string('kode_kriteria', 255); // Kode kriteria utama
+      $table->string('tipe_penilaian', 255); // Tipe penilaian (string, nominal, range)
+      $table->string('parameter', 255)->nullable(); // Parameter (string, nullable)
+      $table->integer('parameter_nominal')->nullable(); // Nominal parameter (optional)
+      $table->integer('parameter_min')->nullable(); // Minimum parameter value
+      $table->integer('parameter_max')->nullable(); // Maximum parameter value
+      $table->integer('nilai'); // Nilai sub kriteria
+      $table->timestamps(); // Created at & Updated at
 
-            // $table->foreign('kode_kriteria')->references('kode_kriteria')->on('kriterias')->onDelete('cascade');
-        });
-    }
+      // Optional: Tambahkan index jika diperlukan
+      $table->index('kode_kriteria');
+    });
+  }
 
-    public function down()
-    {
-        Schema::dropIfExists('sub_kriterias');
-    }
+  public function down()
+  {
+    Schema::dropIfExists('sub_kriterias');
+  }
 }
