55816d75cf5132527ee6e112b3c30d23cf6b4033
adamjatim <adamrahmatilahi@gmail.com>
2024-12-10 20:37:03 +0700

    update: owner & admin have same dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,22 +12,15 @@ use App\Models\DataHasil;
 class DashboardController extends Controller
 {
   public function index()
-  // public function render()
-  // public function __invoke()
   {
     $role = auth()->user()->role;
-    if ($role === 'admin') {
-      // Jika role admin, kembalikan dashboard admin
-      return redirect()->route('calon-pegawai.index');
-    }  elseif ($role === 'owner') {
-      return view('owner.dashboard', [
-        // dd($this->calonPegawais),
-        'calonPegawais' => CalonPegawai::all(),
-        'kriterias' => Kriteria::orderBy('kode_kriteria', 'asc')->get(),
-        'subKriterias' => SubKriteria::all(),
-        'dataHasils' => DataHasil::all(),
-      ]);
-    }
+    return view('owner.dashboard', [
+      // dd($this->calonPegawais),
+      'calonPegawais' => CalonPegawai::all(),
+      'kriterias' => Kriteria::orderBy('kode_kriteria', 'asc')->get(),
+      'subKriterias' => SubKriteria::all(),
+      'dataHasils' => DataHasil::all(),
+    ]);
 
     // Jika role tidak valid, tampilkan error
     abort(403, 'Unauthorized access');
